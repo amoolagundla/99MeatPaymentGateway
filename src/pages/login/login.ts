27dba384ab69b3a0e04167a15c15a4dd7a2b4711
bhavikmehta100 <bhavikmehta100@gmail.com>
2016-12-28 19:56:31 +0530

    Facebook login Code Added

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -10,8 +10,9 @@ import {RegisterPage} from "../register/register";
 import 'rxjs/add/operator/catch';
 import  'rxjs/add/operator/mergeMap'; 
 import * as Rx from 'rxjs/rx';
-import {Facebook, Google, LinkedIn} from "ng2-cordova-oauth/core";
-import {OauthCordova} from 'ng2-cordova-oauth/platform/cordova';
+import { Facebook, NativeStorage } from 'ionic-native';
+/*import {Facebook, Google, LinkedIn} from "ng2-cordova-oauth/core";
+import {OauthCordova} from 'ng2-cordova-oauth/platform/cordova';*/
 declare const facebookConnectPlugin: any;
 /*
  Generated class for the LoginPage page.
@@ -25,12 +26,12 @@ declare const facebookConnectPlugin: any;
 })
 export class LoginPage {
 
-private oauth: OauthCordova = new OauthCordova();
+/*private oauth: OauthCordova = new OauthCordova();
     private facebookProvider: Facebook = new Facebook({
       clientId: "172669669872080",
       appScope: ["email"]
-    })
-		
+    })*/
+    
 public username:string;
 public token:string;
 public loading :any= this.loadingCtrl.create({
@@ -39,14 +40,14 @@ public loading :any= this.loadingCtrl.create({
     });
 public password:string;
   constructor(public nav: NavController, 
-	            private authenticationService: AuthenticationService,
-							public loadingCtrl: LoadingController,
-							public valuesService: ValuesService,
-							public alertCtrl: AlertController,public platform:Platform,)
-							{ 					
-							this.platform = platform;
-								localStorage.removeItem("UserInfo");
-						    localStorage.removeItem('currentUser');				
+              private authenticationService: AuthenticationService,
+              public loadingCtrl: LoadingController,
+              public valuesService: ValuesService,
+              public alertCtrl: AlertController,public platform:Platform,)
+              {           
+              this.platform = platform;
+                localStorage.removeItem("UserInfo");
+                localStorage.removeItem('currentUser');       
   }
 
    // go to register page
@@ -55,51 +56,50 @@ public password:string;
   }
   // login and go to home page
   login() { 
-	
-		this.loading.present();
-		 this.authenticationService.login(this.username, this.password)
+  
+    this.loading.present();
+     this.authenticationService.login(this.username, this.password)
             .subscribe(
                 data => {
-										this.getUserInfo();
-                    
+                    this.getUserInfo();                   
   
                 }, 
                 error => {
-									this.loading.dismiss();
+                  this.loading.dismiss();
                   this.nav.setRoot(LoginPage);
                 });
     
   }
-	
-	
-	getUserInfo()
-	{
-		this.valuesService.getAll()
+  
+  
+  getUserInfo()
+  {
+    this.valuesService.getAll()
             .subscribe(
-                data => {		
-                				this.getCatogories();				
+                data => {   
+                        this.getCatogories();       
                 localStorage.setItem('UserInfo',JSON.stringify(data)); 
                 }, 
                 error => {
-									this.loading.dismiss();
+                  this.loading.dismiss();
                   
                 });
-								
-	}
-	
-	getCatogories()
-	{
-		this.valuesService.getAllCategories()
+                
+  }
+  
+  getCatogories()
+  {
+    this.valuesService.getAllCategories()
             .subscribe(
-                data => {			this.loading.dismiss();							
-		
-		 localStorage.setItem('categories',JSON.stringify(data)); this.nav.setRoot(HomePage);
+                data => {     this.loading.dismiss();             
+    
+     localStorage.setItem('categories',JSON.stringify(data)); this.nav.setRoot(HomePage);
                 }, 
                 error => {
-                  	this.loading.dismiss();
-                });		
-	}
-	ll() {
+                    this.loading.dismiss();
+                });   
+  }
+  /*ll() {
      this.platform.ready().then(() => {
             this.oauth.logInVia(this.facebookProvider).then(success => {
                 console.log("RESULT: " + JSON.stringify(success));
@@ -108,10 +108,36 @@ public password:string;
             });
         });
 }
-	   loginFB() {
+     loginFB() {
        this.ll();
-    }
+    }*/
      
+     FB_APP_ID: number = 1828020577450397;
+
+  loginFB(){
+    let permissions = new Array();
+    let nav = this.nav;
+    //the permissions your facebook app needs from the user
+    permissions = ["public_profile"];
+
+    Facebook.login(permissions)
+    .then(function(response){
+      let userId = response.authResponse.userID;
+      let params = new Array();
+
+      //Getting name and gender properties
+      Facebook.api("/me?fields=name,gender", params)
+      .then(function(user) {
+        user.picture = "https://graph.facebook.com/" + userId + "/picture?type=large";
+        //now we have the users info, let's save it in the NativeStorage
+        alert(JSON.stringify(user));
+      })
+    }, function(error){
+      console.log(error);
+    });
+  }
+
+
     getdetails() {
         facebookConnectPlugin.getLoginStatus((response) => {
             if(response.status == "connected") {
